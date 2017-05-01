76e4e0e25aeae933aa6a2b3d2f1539e53df95494
Michael Martinez <martinez1212@gmail.com>
2017-05-01 10:01:30 -0700

    fixed renaming issue

--- src/screens/LandingPage.js
+++ src/screens/LandingPage.js
@@ -6,45 +6,45 @@ import SearchBar from '../components/SearchBar';
 import { createSearchAction } from '../actions/index';
 
 class LandingPage extends Component {
-  // headerText="Piece Meal"
-  render() {
-    return (
-      <Container>
-        <MyHeader />
-        <InputGroup borderType="regular">
-          <Icon name="md-search" />
-          <Input
-            placeholder="Search"
-            onChangeText={(newText) => this.props.searchRecipe(newText)}
-          />
-          <Button transparent>
-              <Icon name='ios-options' />
-          </Button>
-        </InputGroup>
-        <SearchBar recipes={this.props.recipes} ingredients={this.props.ingredients} />
-      </Container>
-    );
-  }
+ // headerText="Piece Meal"
+ render() {
+   return (
+     <Container>
+       <MyHeader />
+       <InputGroup borderType="regular">
+         <Icon name="md-search" />
+         <Input
+           placeholder="Search"
+           onChangeText={(newText) => this.props.searchRecipe(newText)}
+         />
+         <Button transparent>
+             <Icon name='ios-options' />
+         </Button>
+       </InputGroup>
+       <SearchBar recipes={this.props.recipes} ingredients={this.props.ingredients} />
+     </Container>
+   );
+ }
 }
 
 const mapStateToPropsLandingPage = (state) => {
-  //map state to props to the Landing Page component above
-  return {
-    recipes: state.searchRecipe.recipes,
-    ingredients: state.searchRecipe.ingredients
+ //map state to props to the Landing Page component above
+ return {
+   recipes: state.searchRecipe.recipes,
+   ingredients: state.searchRecipe.ingredients
 
-  };
+ };
 };
 
 const mapDispatchtoPropsLandingPage = (dispatch) => {
-  return {
-    searchRecipe: (recipes) => {
-      dispatch(createSearchAction(recipes));
-    }
-  };
+ return {
+   searchRecipe: (recipes) => {
+     dispatch(createSearchAction(recipes));
+   }
+ };
 };
 
 const ConnectedLandingPage = connect(
-  mapStateToPropsLandingPage, mapDispatchtoPropsLandingPage)(LandingPage);
+ mapStateToPropsLandingPage, mapDispatchtoPropsLandingPage)(LandingPage);
 
 export default ConnectedLandingPage;
