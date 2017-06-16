1985a0badd56b6562ef1d0a1e94bb6a073102069
Michael Martinez <martinez1212@gmail.com>
2017-06-16 09:38:55 -0700

    updated snapshot test; passing

--- __tests__/components/landingPage.test.js
+++ __tests__/components/landingPage.test.js
@@ -1,21 +1,17 @@
 import 'react-native';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { LandingPage } from '../../src/screens/LandingPage';
 import renderer from 'react-test-renderer';
-import { shallow } from 'enzyme';
-import { shallowToJson } from 'enzyme-to-json';
-import promiseMiddleware from 'redux-promise-middleware';
-import configureMockStore from 'redux-mock-store';
-import rootReducer from '../../src/reducers';
-import ConnectedLandingPage from '../../src/screens/LandingPage';
 
-const middlewares = [promiseMiddleware];
-const mockStore = configureMockStore(middlewares);
-
-test('LandingPage should render', () => {
-  const store = mockStore({ rootReducer });
-  const landingPage = shallow(
-    <ConnectedLandingPage store={store} />,
-  );
-  expect(shallowToJson(landingPage)).toMatchSnapShot();
+describe('tests the LandingPage component', () => {
+  it('renders LandingPage component properly', () => {
+    expect(renderer.create(
+      <LandingPage
+        searchRecipe={jest.fn}
+        navigation={'navigation'}
+        ingredients={[]}
+        recipes={[]}
+      />
+    )).toMatchSnapshot();
+  });
 });
