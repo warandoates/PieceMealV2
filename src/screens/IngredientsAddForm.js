00b413ecef0d113a78577e02c0e849645725343d
Michael Martinez <martinez1212@gmail.com>
2017-06-18 16:54:49 -0700

    removed clean native base code

--- src/screens/IngredientsAddForm.js
+++ src/screens/IngredientsAddForm.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
+import { Container, Button, Form, Input, Text, Item } from 'native-base';
+import { reduxForm, Field } from 'redux-form';
 import Toast from 'react-native-simple-toast';
 import { connect } from 'react-redux';
 import {
   ActionsContainer,
-  Button,
-  FieldsContainer,
   Fieldset,
-  Form,
 } from 'react-native-clean-form';
-import { Input, Select, } from 'react-native-clean-form/redux-form';
+import { Select } from 'react-native-clean-form/redux-form';
 import { postIngredient } from '../actions';
 
 class AddIngredientForm extends Component {
+
+  renderInput({ input, label, type }) {
+    return ( <Item >
+                        <Input {...input}/>
+                    </Item> )
+  }
+
   onSubmit(ingredient) {
     return this.props.postIngredient(ingredient, this.props.token)
     .then((res) => {
@@ -34,29 +39,29 @@ class AddIngredientForm extends Component {
       return { label: ingredient.name, key: ingredient.id, value: ingredient.id };
     });
     return (
-      <Form>
-        <FieldsContainer>
-          <Fieldset label="Ingredient Details">
-            <Input name="name" label="Ingredient Name" placeholder="Honeycrisp Apple" />
-            <Input name="description" label="Description" placeholder="The most delicious apple" />
-          </Fieldset>
-          <Fieldset label="Extras" last>
-            <Select
+      <Form style={{flex: 1}} >
+        <Container style={{flex: 1}} >
+          <Field style={{flex: 1}} name="name" label="Ingredient Details" type='text' placeholder="Honeycrisp Apple" component={this.renderInput({label: 'example'})} />
+            {/* <Input  label="Ingredient Name" placeholder="Honeycrisp Apple" />
+            <Input name="description" label="Description" placeholder="The most delicious apple" /> */}
+          {/* </Field> */}
+          {/* <Field label="Extras" last> */}
+            {/* <Select
               name="alternatives"
               label="alternatives"
               placeholder="Oranges?"
               options={ingredientOptions}
               // onChange={(value)}
-            />
-            <Input name="tags" label="Tags" placeholder="crisp" />
-            <Input name="photos" label="photos" placeholder="Photo?" />
-          </Fieldset>
-        </FieldsContainer>
-        <ActionsContainer>
+            /> */}
+            {/* <Input name="tags" label="Tags" placeholder="crisp" />
+            <Input name="photos" label="photos" placeholder="Photo?" /> */}
+          {/* </Field> */}
+        </Container>
+        <Container>
           <Button onPress={handleSubmit(this.onSubmit.bind(this))} icon="md-checkmark" iconPlacement="right" submitting={submitting} >
-            Submit
+            <Text>Submit</Text>
           </Button>
-        </ActionsContainer>
+        </Container>
       </Form>
     );
   }
