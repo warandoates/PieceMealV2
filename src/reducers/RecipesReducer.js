d3efcb52c1f9a15573cd8ccb5e85e8726163bcf4
Michael Martinez <martinez1212@gmail.com>
2017-06-20 00:46:53 -0700

    fixed linting issues

--- src/reducers/RecipesReducer.js
+++ src/reducers/RecipesReducer.js
@@ -3,10 +3,7 @@ const INITIAL_STATE = { recipes: [{}] };
 const recipesResults = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case 'GET_RECIPE_RESULTS_PENDING':
-    return Object.assign({}, state, {
-      isFetching: true,
-      didInvalidate: false
-    });
+      return { ...state, isFetching: true, didInvalidate: false };
     case 'GET_RECIPE_RESULTS_FULFILLED':
       return action.payload;
     default:
