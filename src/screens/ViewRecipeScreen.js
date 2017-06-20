609bd300fde2bcb3c628d0768646601cdd3ea3a1
Michael Martinez <martinez1212@gmail.com>
2017-06-19 22:08:48 -0700

    linted code

--- src/screens/ViewRecipeScreen.js
+++ src/screens/ViewRecipeScreen.js
@@ -5,6 +5,9 @@ import RecipeView from '../components/Recipe/RecipeView';
 export default class ViewRecipeScreen extends Component {
   render() {
     const params = this.props.navigation.state.params;
-    return <Container><RecipeView recipe={params.recipe} /></Container>;
+    return (
+      <Container>
+        <RecipeView recipe={params.recipe} />
+      </Container>);
   }
 }
